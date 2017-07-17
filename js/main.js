b60ee379fe15db8c04ae565aee7817e32724c4fd
devincosta <devincostaphoto@gmail.com>
2017-07-17 00:09:32 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
-console.log("Up and running!");
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
 
-console.log("User flipped" + " " + cardOne);
-console.log("User flipped" + " " + cardThree);
+'use strict'
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
+var cardTwo = cards[3];
+cardsInPlay.push('cardTwo');
+console.log("User Flipped King");
+
+
+//Testing
+
+if(cardsInPlay.length === 3){
+  alert("You found a match");
+} else {
+  alert("Sorry, try again");
+}
+
+
+// //my original code
+// if (cardsInPlay.length === 2) {
+//   function ()
+//     console.log("You found a match");
+// } else if (cardsInPlay[0 === cardsInPlay[2]); {
+//     // greeting = "Sorry, try again";
+//     console.log("No");
+
+
+
+    /*if (cardsInPlay.length === 2) {
+    alert = "You found a match";
+    }
+    else if {cardsInPlay[0] === cardsInPlay[2]
+    alert ="Sorry, try again";}*/
