750dc3e280340736c95ade864e115b8c234b3d7e
devincosta <devincostaphoto@gmail.com>
2017-07-20 17:52:43 -0400

    Js attempt 2
    i've broken down and started my whole js script over from scratch
    i was originally working off someone elses code but that approuch
    was confusing me more than anything

---
+++ try2.js
@@ -0,0 +1,8 @@
+'use strict'
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[1];
+cardsInPlay.push(cards[1]);
+console.log('user flipped queen');
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo)
